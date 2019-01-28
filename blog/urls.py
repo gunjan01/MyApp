5c86fce280b50262cbe8de62632e0ff0edd30d2f
gunjan01 <gunjan.tank9@gmail.com>
2019-01-28 19:13:18 +0530

    Add a view which renders

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+# importing a Django function called path.
+# Importing everything from views.
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
+
+# '' is resolved as localhost.
+# we're now assigning a view called post_list to the root URL
+# The last part, name='post_list', is the name of the URL that will be used to identify the view. ?
