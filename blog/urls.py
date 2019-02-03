aa9c3a66d653c1411087d563e9c7e5b866c78996
gunjan01 <gunjan.tank9@gmail.com>
2019-02-03 18:03:53 +0530

    Add another url and render blog details

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+	 path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
 
 # '' is resolved as localhost.
